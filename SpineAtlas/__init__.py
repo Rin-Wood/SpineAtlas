c0ed4dea009fe11be0260077b37c7d78fba2ad55
wood <153263099+Rin-Wood@users.noreply.github.com>
2025-02-25 17:45:43 +0800

    Update __init__.py

--- SpineAtlas/__init__.py
+++ SpineAtlas/__init__.py
@@ -1,1 +1,1 @@
-from .Spine_Atlas import Anchor, Atlas, AtlasFrame, AtlasTex, SpineAtlas, LineTextReader, rbin, sline, AtlasScale, ReOffset, CutFrame, AtlasImg, ImgPremultiplied, ImgNonPremultiplied
+from .Spine_Atlas import Anchor, Atlas, AtlasFrame, AtlasTex, SpineAtlas, LineTextReader, rbin, sline, AtlasScale, CutFrame, AtlasImg, ImgPremultiplied, ImgNonPremultiplied, ReadAtlas, ReadAtlasFile
