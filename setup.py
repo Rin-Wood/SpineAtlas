01f5983f7fb8d91b2810ffed7ebd3244513c9b1c
wood <153263099+Rin-Wood@users.noreply.github.com>
2026-01-20 06:58:49 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="SpineAtlas",
-    version="1.0.11",
+    version="1.0.12",
     author='wood',
     author_email='miraclerinwood@gmail.com',
     packages=["SpineAtlas"],
